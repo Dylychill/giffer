ec1f543adf8c71359f7de4b59d4f535959b9562c
Dylychill <demckone@gmail.com>
2020-07-21 17:11:56 -0700

    fetch gifs from giphy API

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ import Button from '@material-ui/core/Button';
 
 function App() {
   const [text,setText] = useState('')
+  const [memes,setMemes] = useState([])
+
+  // Make the fetches inside this function so it isn't rerun at EVERY change
+  async function search(){
+    const key = '5EeHryunuSqGlfmpDTv5MKRoyMygHpGm'
+    // this is bad style to have the key in public
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${text}&limit=25&offset=0&lang=en`
+    // Use backticks above to incluse the key and text
+    const resp = await fetch(url)
+    const jaseResp = await resp.json()
+    setMemes(jaseResp.data)
+    setText('') //empty the search bar after enter
+  }
 
   return (
     <div className="App">
@@ -22,6 +35,12 @@ function App() {
         <Button variant="contained" color="primary" style={{height:50, marginLeft:8}} disabled={!text} onClick={search}>
             Search!
         </Button>
+      </div>
+      <div className="memes">
+        {memes.map((m,i)=>{
+          return <img key={i} src={m.images.fixed_height.url}/>
+          // fixed_height chosen from the list. consolelog to see what is available.
+        })}
       </div>
     </div>
   );
